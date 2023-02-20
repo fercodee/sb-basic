e4d0dad16bca5de9ceafaf85cd3b00f46bd8c7e3
fercodee <devbyfull@gmail.com>
2023-02-20 04:58:12 -0300

    feat: add getAllProducts, getProductById

--- src/main/java/com/example/exercisessb/controllers/ProductController.java
+++ src/main/java/com/example/exercisessb/controllers/ProductController.java
@@ -1,6 +1,12 @@
 package com.example.exercisessb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +23,19 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @PostMapping
-    public @ResponseBody Product newProduct(Product product) {
+    public @ResponseBody Product newProduct(@Valid Product product) {
         productRepository.save(product);
         return product;
+    }
+
+    @GetMapping
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Product> getProductById(@PathVariable int id) {
+        return productRepository.findById(id);
     }
 
 }
--- src/main/java/com/example/exercisessb/model/entities/Product.java
+++ src/main/java/com/example/exercisessb/model/entities/Product.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Product {
@@ -11,8 +14,15 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    
+    @NotBlank
     private String name;
+    
+    @Min(0)
     private double price;
+    
+    @Min(0)
+    @Max(1)
     private double discount;
 
     public Product() {
